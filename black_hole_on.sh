00ad8ba0391b5a2dff97c06b176d01fc515bba3c
Luck <BigbigY@users.noreply.github.com>
2017-09-07 20:17:16 +0800

    Update black_hole_on.sh

--- black_hole_on.sh
+++ black_hole_on.sh
@@ -3,7 +3,7 @@
 
 wight_num="$1"
 # 获取主机列表
-var=$(curl http://yun.ops.bbtree.com/mtree/get_hostnames?treeid=2198)
+var=$(curl http://xxx.xxxx.xxx/mtree/get_hostnames?treeid=2198)
 host_list=`echo $var|sed 's/,/ /g;s/\[/ /g;s/\]/ /g;s/"/ /g'`
 for remote_host in $host_list;do
     /usr/bin/salt $remote_host cmd.script salt://scripts/phpwannx/black_hole_on_client.sh $wight_num
